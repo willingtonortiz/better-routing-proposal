3756ab3f862f02e076328d8fdc424aa9cf752d19
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2023-03-09 22:49:33 -0500

    Add comments to explain the porpose

--- src/routes/product-routes.ts
+++ src/routes/product-routes.ts
@@ -1,16 +1,25 @@
 export class ProductRoutes {
-  // React router paths
+  /* *
+   * React router routes
+   * Used only in the router component
+   * */
   static readonly INDEX = `/products`;
   static readonly ROOT = `${ProductRoutes.INDEX}/*`;
   static readonly PRODUCT_DETAILS = `:productId`;
   static readonly ADD_PRODUCT = `add`;
   static readonly EDIT_PRODUCT = `:productId/edit`;
   static readonly DELETE_PRODUCT = `:productId/delete`;
-  static readonly PRODUCT_LIST = `/products`;
 
+  /* *
+   * Base path
+   * Used as the base path for route methods
+   */
   private static getBasePath = () => `${ProductRoutes.INDEX}`;
 
-  // Route methods
+  /* *
+   * Route methods
+   * Used in the components to generate paths
+   * */
   static productList = () => ProductRoutes.getBasePath();
   static productDetail = (productId: string) =>
     `${ProductRoutes.getBasePath()}/:productId`.replace(
